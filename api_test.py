a82eaf12db58e878441dfd2a97538c648d49ed04
bogyu-jang <enprobk@gmail.com>
2014-11-24 11:40:53 +0900

    try, except added
    try, except added

--- api_test.py
+++ api_test.py
@@ -1,7 +1,18 @@
-import urllib
+import urllib2
 
 url = 'http://203.247.39.98/'
 
+try:
+	data = urllib2.urlopen(url).read()
+except urllib2.HTTPError, e:
+	print "HTTP error : %d" % e.code
+except urllib2.URLError, e:
+	print "Network Error: %s" % e.reason.args[1]
+
+	
+
+"""
 u = urllib.urlopen(url)
 data = u.read()
+"""
 
